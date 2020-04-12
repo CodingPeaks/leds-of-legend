f20d2f9e06bae074e27fad7dfb9bfd8953835993
Marco Nardone <39136442+CodingPeaks@users.noreply.github.com>
2020-04-12 15:53:23 +0200

    Allora, da capo...
    Andiamo con ordine

--- ledcontroller/ledcontroller.ino
+++ ledcontroller/ledcontroller.ino
@@ -4,28 +4,19 @@
 #define Sprint(a) (Serial.print(a))
 //#define Sprintln(a)
 //#define Sprint(a)
-#define fstep 10
+
 String sdata = "";
-
-byte target_r = 0;
-byte target_g = 0;
-byte target_b = 0;
-
-unsigned long fade = 10;
 byte r = 0;
 byte g = 0;
 byte b = 0;
 
-unsigned long curb = 0;
-unsigned long curtime = 0;
-
-#define LEDPIN 22
+#define LEDPIN D3
 #define NUMPIXELS 151
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 void setup (void) {
-  Serial.begin(115200);
+  Serial.begin(921600);
   Sprintln("Command Interpreter");
   pixels.begin();
 }
@@ -35,7 +26,6 @@ void loop() {
   int i = 0;
 
   if (Serial.available()) {
-    curtime = micros();
     ch = Serial.read();
     sdata += (char)ch;
     if (ch == '\r') {
@@ -58,11 +48,6 @@ void loop() {
             Sprint("b: ");
             Sprintln(b);
             break;
-          case 3:
-            fade = got.toInt();
-            Sprint("f: ");
-            Sprintln(fade);
-            break;
         }
         i++;
       } while (getValue(sdata, ';', i) != "");
@@ -71,51 +56,14 @@ void loop() {
     }
   }
 
-  fadeTo();
-
-}
-
-void fadeTo() {
-
-
-  if (micros() - curtime > fade) {
-
-    if (target_r < r && !(abs(target_r - r) < fstep)) {
-      target_r = target_r + fstep;
-    } else if (target_r > r && !(abs(target_r - r) < fstep)) {
-      target_r = target_r - fstep;
-    }
-
-    if (target_g < g && !(abs(target_g - g) < fstep)) {
-      target_g = target_g + fstep;
-    } else if (target_g > g && !(abs(target_g - g) < fstep)) {
-      target_g = target_g - fstep;
-    }
-
-    if (target_b < b && !(abs(target_b - b) < fstep)) {
-      target_b = target_b + fstep;
-    } else if (target_b > b && !(abs(target_b - b) < fstep)) {
-      target_b = target_b - fstep;
-    }
-
-    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-      pixels.setPixelColor(i, pixels.Color(r, g, b));
-    }
-
-    Serial.print(target_r);
-    Serial.print(" ");
-    Serial.print(target_g);
-    Serial.print(" ");
-    Serial.println(target_b);
-
-    pixels.show();
-
-    curtime = micros();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(r, g, b));
   }
+  pixels.show();
+
 }
 
-String getValue(String dat, char separator, int index)
-{
+String getValue(String dat, char separator, int index) {
   int found = 0;
   int strIndex[] = {0, -1};
   int maxIndex = dat.length() - 1;
