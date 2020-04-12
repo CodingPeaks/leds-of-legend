e6e9b4ecb92fc4351f1d41259197d70464bb7600
Marco Nardone <39136442+CodingPeaks@users.noreply.github.com>
2020-04-12 17:22:46 +0200

    Update ledcontroller.ino

--- ledcontroller/ledcontroller.ino
+++ ledcontroller/ledcontroller.ino
@@ -9,6 +9,10 @@ String sdata = "";
 byte r = 0;
 byte g = 0;
 byte b = 0;
+byte r_target = 0;
+byte g_target = 0;
+byte b_target = 0;
+int f = 30;
 
 #define LEDPIN D3
 #define NUMPIXELS 151
@@ -16,7 +20,7 @@ byte b = 0;
 Adafruit_NeoPixel pixels(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 void setup (void) {
-  Serial.begin(921600);
+  Serial.begin(115200);
   Sprintln("Command Interpreter");
   pixels.begin();
 }
@@ -34,19 +38,24 @@ void loop() {
         String got = getValue(sdata, ';', i);
         switch (i) {
           case 0:
-            r = got.toInt();
-            Sprint("r: ");
-            Sprintln(r);
+            r_target = got.toInt();
+            Sprint("r_target: ");
+            Sprintln(r_target);
             break;
           case 1:
-            g = got.toInt();
-            Sprint("g: ");
-            Sprintln(g);
+            g_target = got.toInt();
+            Sprint("g_target: ");
+            Sprintln(g_target);
             break;
           case 2:
-            b = got.toInt();
-            Sprint("b: ");
-            Sprintln(b);
+            b_target = got.toInt();
+            Sprint("b_target: ");
+            Sprintln(b_target);
+            break;
+          case 3:
+            f = got.toInt();
+            Sprint("f: ");
+            Sprintln(f);
             break;
         }
         i++;
@@ -60,6 +69,39 @@ void loop() {
     pixels.setPixelColor(i, pixels.Color(r, g, b));
   }
   pixels.show();
+
+  fade();
+}
+
+void fade() {
+
+  int stp = map(r, 1, 255, 1, f);
+
+  if (r_target > r + stp) {
+    r = r + stp;
+  } else if (r_target <= r - stp) {
+    r = r - stp;
+  } else {
+    r = r_target;
+  }
+
+  if (g_target > g + stp) {
+    g = g + stp;
+  } else if (g_target <= g - stp) {
+    g = g - stp;
+  } else {
+    g = g_target;
+  }
+
+  if (b_target > b + stp) {
+    b = b + stp;
+  } else if (b_target <= b - stp) {
+    b = b - stp;
+  } else {
+    b = b_target;
+  }
+
+  Serial.println("Fading in with step -> " + String(stp) + " | r -> " + String(r));
 
 }
 
