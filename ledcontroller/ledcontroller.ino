b72d9df536564d5fed42c55e08f5056b56186a75
Marco Nardone <39136442+CodingPeaks@users.noreply.github.com>
2020-04-14 09:51:55 +0200

    API http
    Siamo integratori, usiamo le API

--- ledcontroller/ledcontroller.ino
+++ ledcontroller/ledcontroller.ino
@@ -1,10 +1,15 @@
 #include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
 #define Sprintln(a) (Serial.println(a))
 #define Sprint(a) (Serial.print(a))
 //#define Sprintln(a)
 //#define Sprint(a)
 
+ESP8266WebServer server(80);
+int rhtt, ghtt, bhtt;
 String sdata = "";
 float r = 0;
 float g = 0;
@@ -14,23 +19,89 @@ float g_target = 0;
 float b_target = 0;
 float f = 30;
 bool flag = true;
+bool rcvhtt = true;
 
 #define LEDPIN D3
 #define NUMPIXELS 151
+#define STASSID "contact1"
+#define STAPSK  "0210111966"
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+void handleGenericArgs() { //Handler
+
+  rcvhtt = true;
+
+  String message = "Number of args received: ";
+  message += server.args();            //Get number of parameters
+  message += "\n";                            //Add a new line
+
+  for (int i = 0; i < server.args(); i++) {
+
+    message += "Arg nº" + (String)i + " – > ";  //Include the current iteration value
+    message += server.argName(i) + ": ";     //Get the name of the parameter
+    message += server.arg(i) + "\n";              //Get the value of the parameter
+
+    if (server.argName(i) == "r") {
+      rhtt = server.arg(i).toInt();
+    }
+
+    if (server.argName(i) == "g") {
+      ghtt = server.arg(i).toInt();
+    }
+
+    if (server.argName(i) == "b") {
+      bhtt = server.arg(i).toInt();
+    }
+
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(rhtt, ghtt, bhtt));
+    }
+    pixels.show();
+
+  }
+
+  server.send(200, "text / plain", message);     //Response to the HTTP request
+
+}
 
 void setup (void) {
   Serial.begin(115200);
   Sprintln("Command Interpreter");
   pixels.begin();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleGenericArgs);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
 }
 
 void loop() {
+  server.handleClient();
   byte ch;
   int i = 0;
 
   if (Serial.available()) {
+    rcvhtt = false;
     ch = Serial.read();
     sdata += (char)ch;
     if (ch == '\r') {
@@ -67,12 +138,14 @@ void loop() {
     }
   }
 
-  for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color((int) r, (int) g, (int) b));
-  }
-  pixels.show();
+  if (!rcvhtt) {
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color((int) r, (int) g, (int) b));
+    }
+    pixels.show();
 
-  fade();
+    fade();
+  }
 }
 
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
@@ -92,7 +165,7 @@ void fade() {
     r = r - stp;
   } else {
     r = r_target;
-    
+
   }
 
   if (g_target > g + stp) {
@@ -111,7 +184,7 @@ void fade() {
     b = b_target;
   }
 
-  if(r == r_target && g == g_target && b == b_target && flag){
+  if (r == r_target && g == g_target && b == b_target && flag) {
     Serial.println("Fine");
     flag = false;
   }
